cf210a3db2ff7a8f809ff2df841eb98c124e7eb1
wangshiphys <wangshiphys@gmail.com>
2017-02-27 21:49:15 +0800

    Add constant SWAP_FACTOR_F and SWAP_FACTOR_B.

--- HamiltonianPy/constant.py
+++ HamiltonianPy/constant.py
@@ -8,3 +8,7 @@ ANNIHILATION = 0
 
 #Absolute value less than this quantity is viewed as 0.
 VIEW_AS_ZERO = 1E-8
+
+#The coefficience that swap two fermion or boson.
+SWAP_FACTOR_F = -1
+SWAP_FACTOR_B = 1
