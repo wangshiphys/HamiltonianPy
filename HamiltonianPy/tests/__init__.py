8f8dd6a99ac99c32f42b10ca162e304e7a484e0a
wangshiphys <wangshiphys@gmail.com>
2019-04-15 11:02:55 +0800

    Add `tests` subpackage which contains unit and integration tests for this project

---
+++ HamiltonianPy/tests/__init__.py
@@ -0,0 +1,15 @@
+"""
+tests
+=====
+
+Provides
+  1. Unit test scripts for modules defined in this project
+  2. Integration test scripts for this project
+
+Available subpackages
+---------------------
+unit
+    Unit test scripts for modules defined in this project
+integration
+    Integration test scripts for this project
+"""
