3d05713c3b7287a5243026f02e4dbd1e9069f719
wangshiphys <wangshiphys@gmail.com>
2019-04-15 11:03:50 +0800

    Add `unit` subpackage which contains unit-tests for modules defined in this project

---
+++ HamiltonianPy/tests/unit/__init__.py
@@ -0,0 +1,34 @@
+"""
+unit
+=====
+
+Provides
+  Unit test scripts for modules defined in this project
+
+Available modules
+---------------------
+test_AoC
+    A test script for the AoC class
+test_bond
+    A test script for the bond module
+test_hilbertspace
+    A test script for the hilbertspace module
+test_indextable
+    A test script for the indextable module
+test_lanczos
+    A test script for the lanczos module
+test_lattice
+    A test script for the lattice module
+test_matrixrepr
+    A test script for the matrixrepr module
+test_ParticleTerm
+    A test script for the ParticleTerm class
+test_SiteID
+    A test script for the SiteID class
+test_SpinInteraction
+    A test script for the SpinInteraction class
+test_SpinOperator
+    A test script for the SpinOperator class
+test_StateID
+    A test script for the StateID class
+"""
