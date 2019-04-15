740aadbdb8a29db74a3b834c25a0a2c8c55dcd61
wangshiphys <wangshiphys@gmail.com>
2019-04-15 11:20:42 +0800

    Make the `tools` directory as a subpackage

---
+++ HamiltonianPy/tools/__init__.py
@@ -0,0 +1,22 @@
+"""
+tools
+=====
+
+Provides
+  Some useful tools
+
+Available modules
+-----------------
+BrillouinZone
+    Draw the first Brillouin zone for triangle, honeycomb and kagome lattice
+colorprint
+    ANSI Color formatting for output in terminal
+Draw2DLattice
+    Draw some commonly used 2D lattice
+Octahedron
+    Plot the octahedron in 3D
+runtimeenv
+    Temporary run time environment for python program
+TotalSpin
+    Calculate the total spin matrices of a system
+"""
