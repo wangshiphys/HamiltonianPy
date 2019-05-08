874e860fa418ab4038d7c8e79b0693d09faef105
wangshiphys <wangshiphys@gmail.com>
2019-05-08 14:22:30 +0800

    Modify the test script for the `SiteID` class

--- HamiltonianPy/tests/unit/test_SiteID.py
+++ HamiltonianPy/tests/unit/test_SiteID.py
@@ -2,28 +2,28 @@
 A test script for the SiteID class
 """
 
+
 import numpy as np
 import pytest
 
 from HamiltonianPy.indextable import IndexTable
-from HamiltonianPy.termofH import SiteID
+from HamiltonianPy.quantumoperator.quantumstate import SiteID
 
 
 def test_init():
     with pytest.raises(AssertionError):
-        SiteID(site=[0, 0])
+        SiteID(site=[0, 0, 0, 0])
     with pytest.raises(AssertionError):
-        SiteID(site=np.array([0, 0, 0, 0]))
+        SiteID(site=[0, "a"])
 
-    site = np.array([1.2, 2.3])
-    site_id = SiteID(site=site)
-    assert np.all(site_id.site == site)
-    assert repr(site_id) == "SiteID(site=array([1.2, 2.3]))"
+    site_id = SiteID(site=[1.2, 2.3])
+    assert np.all(site_id.site == (1.2, 2.3))
+    assert repr(site_id) == "SiteID(site=(1.2, 2.3))"
 
 
 def test_hash():
-    site_id0 = SiteID(site=np.array([0, 0], dtype=np.int64))
-    site_id1 = SiteID(site=np.array([1E-10, 1E-8], dtype=np.float64))
+    site_id0 = SiteID(site=[0, 0])
+    site_id1 = SiteID(site=[1E-10, 1E-8])
     assert hash(site_id0) == hash(site_id1)
 
 
@@ -48,7 +48,7 @@ def test_comparison():
     assert site_id0 < site_id1
     assert site_id1 < site_id2
     assert site_id0 < site_id2
-    assert site_id0 == SiteID(site=np.array([1e-8, 1e-8]))
+    assert site_id0 == SiteID(site=[1e-8, 1e-8])
     assert site_id0 != site_id1
 
 
