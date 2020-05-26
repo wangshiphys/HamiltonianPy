5886b5a1a89e50eeaf6eeedfdd07d926499992da
wangshiphys <wangshiphys@gmail.com>
2020-05-26 18:48:22 +0800

    Some changes to the docstring.

--- HamiltonianPy/GreenFunction/__init__.py
+++ HamiltonianPy/GreenFunction/__init__.py
@@ -11,11 +11,11 @@ GFSolverExactSingle
 GFSolverExactMultiple
     Calculate Green-Function terms $G_{AB}(z)$ for all `As`, `Bs` and `omegas`.
 GFSolverLanczosSingle
-    Calculate the Green-Function term $G_{AB}(z)=<GS| A M^{1} B |GS>$.
+    Calculate the Green-Function term $G_{AB}(z) = <GS| A M^{-1} B |GS>$.
 GFSolverLanczosMultiple
     Calculate Green-Function terms $G_{AB}(z)$ for all `As`, `Bs` and `omegas`.
 RGFSolverExactSingle
-    Calculate the retarded Green-Function: $G_{AB}^{r}(z)$.
+    Calculate the retarded Green-Function $G_{AB}^{r}(z)$.
 RGFSolverExactMultiple
     Calculate retarded Green-Functions $G_{AB}^{r}(z)$ for all `As`, `Bs` and `omegas`.
 RGFSolverLanczosSingle
